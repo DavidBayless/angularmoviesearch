158a4950b5cee769a8075868aa95a7ee7aef4030
David Bayless <David.C.Bayless15@gmail.com>
2016-01-26 15:12:42 -0700

    made ajax calls to omdb api using angular http

--- js/controllers.js
+++ js/controllers.js
@@ -5,13 +5,32 @@ function MovieController($http) {
 
   var config = {
     method: 'GET',
-    url: 'http://www.omdbapi.com/?t=Frozen&y=&plot=short&r=json'
+    url: 'http://www.omdbapi.com/?s=Frozen&y=&plot=short&r=json'
   };
 
   $http(config)
     .then(function(response) {
       console.log(response);
       vm.infographic = response;
+      var movieChain = [];
+      for (var i = 0; i < response.data.Search.length; i++) {
+        movieChain.push(response.data.Search[i]);
+      }
+      movieChain.forEach(function(movie, index) {
+        var newConfig = {
+          method: 'GET',
+          url: 'http://www.omdbapi.com/?t='+movie.Title+'&y='+movie.Year+'&r=json'
+        };
+
+        $http(newConfig)
+        .then(function(res) {
+          vm.infographic.data.Search[index].imdbRating = res.data.imdbRating;
+          vm.infographic.data.Search[index].Rated = res.data.Rated;
+          if (vm.infographic.data.Search[index].Poster === 'N/A') {
+            vm.infographic.data.Search[index].Poster = 'http://autopartsguy.com/media/no_product.png';
+          }
+        });
+      });
     })
     .catch(function(response) {
       vm.infographic = response;
