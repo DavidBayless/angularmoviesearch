2aef312bf53e77209c5ff90b36f0957a889c99bd
David Bayless <David.C.Bayless15@gmail.com>
2016-01-26 16:09:23 -0700

    rendered data from angular xhr to page

--- js/controllers.js
+++ js/controllers.js
@@ -3,36 +3,44 @@ app.controller('MovieController', ['$http', MovieController]);
 function MovieController($http) {
   var vm = this;
 
-  var config = {
+  vm.filter = 'imdbRating';
+  vm.config = {
     method: 'GET',
-    url: 'http://www.omdbapi.com/?s=Frozen&y=&plot=short&r=json'
+    url: ''
   };
 
-  $http(config)
-    .then(function(response) {
-      console.log(response);
-      vm.infographic = response;
-      var movieChain = [];
-      for (var i = 0; i < response.data.Search.length; i++) {
-        movieChain.push(response.data.Search[i]);
-      }
-      movieChain.forEach(function(movie, index) {
-        var newConfig = {
-          method: 'GET',
-          url: 'http://www.omdbapi.com/?t='+movie.Title+'&y='+movie.Year+'&r=json'
-        };
+  vm.search = function(title) {
+    vm.config.url = 'http://www.omdbapi.com/?s='+title+'&r=json';
+    query(vm.config);
+  };
 
-        $http(newConfig)
-        .then(function(res) {
-          vm.infographic.data.Search[index].imdbRating = res.data.imdbRating;
-          vm.infographic.data.Search[index].Rated = res.data.Rated;
-          if (vm.infographic.data.Search[index].Poster === 'N/A') {
-            vm.infographic.data.Search[index].Poster = 'http://autopartsguy.com/media/no_product.png';
-          }
+  function query(config) {
+    $http(config)
+      .then(function(response) {
+        console.log(response);
+        vm.infographic = response;
+        var movieChain = [];
+        for (var i = 0; i < response.data.Search.length; i++) {
+          movieChain.push(response.data.Search[i]);
+        }
+        movieChain.forEach(function(movie, index) {
+          var newConfig = {
+            method: 'GET',
+            url: 'http://www.omdbapi.com/?t='+movie.Title+'&y='+movie.Year+'&r=json'
+          };
+
+          $http(newConfig)
+          .then(function(res) {
+            vm.infographic.data.Search[index].imdbRating = res.data.imdbRating;
+            vm.infographic.data.Search[index].Rated = res.data.Rated;
+            if (vm.infographic.data.Search[index].Poster === 'N/A') {
+              vm.infographic.data.Search[index].Poster = 'http://autopartsguy.com/media/no_product.png';
+            }
+          });
         });
+      })
+      .catch(function(response) {
+        vm.infographic = response;
       });
-    })
-    .catch(function(response) {
-      vm.infographic = response;
-    });
+  }
 }
